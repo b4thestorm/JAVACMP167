d106c45df553ab284a23b4998b4319b2b8b5851a
arnoldsanders <arnoldsander@gmail.com>
2023-04-29 17:42:11 -0400

    auto resize array

--- readGrades.java
+++ readGrades.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class MyClass {
     public static int readGrades() {
@@ -15,11 +16,24 @@ public class MyClass {
                 grades[idx] = input;
                 idx = idx + 1;
             }
+            
+            if (idx > grades.length - 1) {
+                //make a copy of grades
+                int [] tempArray = new int [grades.length];
+                tempArray = Arrays.copyOf(grades, grades.length);
+                //resize the array to handle more inputs
+                grades = new int[grades.length * 2];
+                //add back original values into the new grades array
+                for (int i = 0; i < tempArray.length; i++) {
+                    grades[i] = tempArray[i];
+                }
+                idx = idx + 1;
+            }
         }
         
         return idx;
     }
-
+    
     public static void main(String args[]) {
         readGrades();
     }
