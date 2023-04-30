9e9023b11129d72e297d53395e0255ae7e9d65e1
arnoldsanders <arnoldsander@gmail.com>
2023-04-30 11:04:23 -0400

    rotate array

--- readGrades.java
+++ readGrades.java
@@ -200,8 +200,16 @@ public class MyClass {
         return count;
     }
 
+    public static void rotateElements(int[] arr) {
+        int temp = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        
+        arr[arr.length - 1] = temp;
+    }
 
-       
+
     public static void main(String args[]) {
 
     }
